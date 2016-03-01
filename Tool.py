c9cf25023d7cf7acb558ce09ddb10f4ab09a6b20
Simon Baron <simon.c.baron@gmail.com>
2016-03-01 15:59:41 +0000

    working on tool to print the details of a record

--- Tool.py
+++ Tool.py
@@ -1,20 +1,43 @@
-'''testings some thgins to see if I can make a tool'''
+'''testings some things to see if I can make a tool'''
 
 from Bio import Entrez
 
 Entrez.email = "simon.c.baron@gmail.com"
 
-handle = Entrez.efetch("pubmed", id="19304878,14630660", retmode="xml")
-records = Entrez.parse(handle)
+def get_record(number):
+
+    handle = Entrez.efetch("pubmed", id=number, retmode="xml")
+    records = Entrez.parse(handle)
+    a = list(records)
+    handle.close()
+    return a
+
+def summarise_record(records):
+    for record in records:
+        print(record['MedlineCitation']['Article']['ArticleTitle'])
+
+def full_info(records):
+    for record in records:
+        for key in record["MedlineCitation"].keys():
+            print key,
+            print "\t"
+            print record["MedlineCitation"][key]
 
 
-count = 0
-for record in records:
-    print(record['MedlineCitation']['Article']['ArticleTitle'])
-    a = record
-    count += 1
+print "starting"
 
-    
-print "hello"
+records = get_record("14716003")#"19304878,14630660")
 
-handle.close()
+summarise_record(records)
+
+full_info(records)
+
+# count = 0
+# for record in records:
+#     print(record['MedlineCitation']['Article']['ArticleTitle'])
+#     a = record
+#     count += 1
+
+
+
+
