a530fbb5537bde3c9bd32bce9c38f0bfb37c34fa
Simon Baron <simon.c.baron@gmail.com>
2016-03-04 16:23:47 +0000

    basic article functionality complete

--- Tool.py
+++ Tool.py
@@ -13,10 +13,12 @@ def get_record(number):
     handle.close()
     return a
 
+
 def summarise_record(records):
     for record in records:
         print(record['MedlineCitation']['Article']['ArticleTitle'])
 
+        
 def full_info(records):
     for record in records:
         for key in record["MedlineCitation"].keys():
@@ -48,7 +50,7 @@ def list_expand(records, st,handle):
 
 def dict_expand(key, records, st, handle):
     st = st + key + "\t"
-    #print key
+    check(key, records)
     if hasattr(records, '__iter__'):
         if isinstance(records, list):
             for item in records:
@@ -57,55 +59,90 @@ def dict_expand(key, records, st, handle):
             for item in records:
                 dict_expand(item, records[item], st, handle)
     else:
-        handle.write(st + "\t" + records +"\n")
+        handle.write(st + "\t" + records + "\n")
 
-def bibtex_string(bibtype, id, bibdict):
-    output = bibtype + "{" + str(id) + ",\n"
-    for key in bibdict:
-        output += "\t" + key + " = {" + bibdict[key] + "},\n"
+        
+def bibtex_string(id, bib_dict):
+    output = "@" + bib_dict["type"] + "{" + str(id) + ",\n"
+    for key in bib_dict:
+        output += "\t" + key + " = {" + bib_dict[key] + "},\n"
     return output + "}"
 
 
-bibdict = {
-    "author" : "",
-    "address" : "",
-    "annote" : "",
-    "booktitle" : "",
-    "chapter" : "",
-    "crossref" : "",
-    "edition" : "",
-    "editor" : "",
-    "howpublished" : "",
-    "institution" : "",
-    "journal" : "",
-    "key" : "",
-    "month" : "",
-    "note" : "",
-    "number" : "",
-    "organization" : "",
-    "pages" : "",
-    "publisher" : "",
-    "school" : "",
-    "series" : "",
-    "title" : "",
-    "type" : "",
-    "volume":"",
-    "year":"",
+def check(key, entry):
+    global ref_dict
+    if key in ref_dict:
+        ref_dict[key] = entry
+
+def dict_convert(ref_dict, bib_dict):
+    author_string = ""
+    for i in range(len(ref_dict["AuthorList"])):
+        author_string += "{} {} and ".format(
+            ref_dict["AuthorList"][i]["ForeName"],
+            ref_dict["AuthorList"][i]["LastName"])
+    bib_dict["author"] =  author_string[:-4]
+    bib_dict["month"] = ref_dict["DateCompleted"]["Month"]
+    bib_dict["year"] = ref_dict["DateCompleted"]["Year"]
+    bib_dict["volume"] = ref_dict["Volume"]
+    bib_dict["title"] = ref_dict["ArticleTitle"]
+    bib_dict["journal"] = ref_dict["Title"]
+    bib_dict["number"] = ref_dict["Issue"]
+    if str(ref_dict["PublicationTypeList"][0]) == "Journal Article":
+        bib_dict["type"] = "article"
+
+
+
+
+
+
+ref_dict = {"AuthorList": "",
+            "DateCompleted": "",
+            "Volume": "",
+            "PublicationTypeList": "",
+            "ArticleTitle": "",
+            "Title": "", # Journal title
+            "Issue": "", # number
+            "MedlinePgn": "",
+}
+
+# check("AuthorList", "hi there!")
+
+bib_dict = {
+    "type": "", ##
+    "author": "", ###
+    "address": "",
+    "annote": "",
+    "booktitle": "",
+    "chapter": "",
+    "crossref": "",
+    "edition": "",
+    "editor": "",
+    "howpublished": "",
+    "institution": "",
+    "journal": "", ##
+    "key": "",
+    "month": "", ###
+    "note": "",
+    "number": "", ##
+    "organization": "",
+    "pages": "", ##
+    "publisher": "",
+    "school": "",
+    "series": "",
+    "title": "", ##
+    "type": "",
+    "volume": "", ###
+    "year": "", ###
 }
 
 print "starting"
 with open('temp.txt', 'w') as f:
-    records = get_record("18716004")#"19304878,14630660")
+    records = get_record("18716004")  # "19304878,14630660")
     st = ""
-    list_expand(records,st,f)
+    list_expand(records, st, f)
 
 print "done"
-#summarise_record(records)
 
-#full_info(records)
+dict_convert(ref_dict, bib_dict)
 
-# count = 0
-# for record in records:
-#     print(record['MedlineCitation']['Article']['ArticleTitle'])
-#     a = record
-#     count += 1
+print bibtex_string("simon", bib_dict)
